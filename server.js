21462bf9c0a602f568a5da9e43e7cad64e160518
Haple <aleph_santos_@hotmail.com>
2017-12-24 09:15:36 -0200

    add index.html and make the server respond to a get request

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('Aspirin at 8h')
+const express = require('express');
+const app = express();
+
+app.listen(3000, function () {
+    console.log('listening on 3000')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
