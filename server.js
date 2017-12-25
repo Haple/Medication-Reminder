f0915fc01bc5da488018e1a6e48019881d6e4f36
Haple <aleph_santos_@hotmail.com>
2017-12-24 23:29:03 -0200

    add index.ejs and enable server show database data

--- server.js
+++ server.js
@@ -15,18 +15,21 @@ MongoClient.connect('mongodb://doctor:doctor3467@ds131687.mlab.com:31687/medicat
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-    db.db('medication-reminder').collection('medications').find().toArray(function(err, results) {
-        console.log(results)
-        // send HTML file populated with quotes here
+    db.db('medication-reminder').collection('medications').find().toArray((err, result) => {
+        if (err) return console.log(err)
+        // renders index.ejs
+        res.render('index.ejs', { medications: result })
     })
 })
 
 app.post('/medications', (req, res) => {
     db.db('medication-reminder').collection('medications').save(req.body, (err, result) => {
         if (err) return console.log(err)
-    
+
         console.log('saved to database')
         res.redirect('/')
-      })
+    })
 })
