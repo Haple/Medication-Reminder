25cb5915a4e537d54a731a351eb4204c7f12820c
Haple <aleph_santos_@hotmail.com>
2017-12-24 23:08:18 -0200

    add connection to database

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 const express = require('express')
-const bodyParser= require('body-parser')
+const bodyParser = require('body-parser')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
 
-app.use(bodyParser.urlencoded({extended: true}))
+var db
 
-app.listen(3000, function () {
-    console.log('listening on 3000')
+MongoClient.connect('mongodb://doctor:doctor3467@ds131687.mlab.com:31687/medication-reminder', (err, database) => {
+    if (err) return console.log(err)
+    db = database
+    app.listen(3000, () => {
+        console.log('listening on 3000')
+    })
 })
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    db.db('medication-reminder').collection('medications').find().toArray(function(err, results) {
+        console.log(results)
+        // send HTML file populated with quotes here
+    })
 })
 
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
+app.post('/medications', (req, res) => {
+    db.db('medication-reminder').collection('medications').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+    
+        console.log('saved to database')
+        res.redirect('/')
+      })
 })
