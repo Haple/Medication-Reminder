7af729c61c9306e408c1e3562d6e97adecc6ce31
Haple <aleph_santos_@hotmail.com>
2018-01-05 22:14:57 -0200

    add user model and make auth routes

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var User = require('/models/user')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -11,7 +15,7 @@ app.use(express.static('public'))
 
 app.use(bodyParser.json())
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 3000))
 
 var db
 
@@ -31,6 +35,89 @@ app.get('/', (req, res) => {
     })
 })
 
+//Routes for user authentication
+app.post('/', (req, res, next) => {
+
+    // confirm that user typed same password twice
+    if (req.body.password !== req.body.passwordConf) {
+        var err = new Error('Passwords do not match.');
+        err.status = 400;
+        res.send("passwords dont match");
+        return next(err);
+    }
+
+    if (req.body.email &&
+        req.body.username &&
+        req.body.password &&
+        req.body.passwordConf) {
+
+        var userData = {
+            email: req.body.email,
+            username: req.body.username,
+            password: req.body.password,
+            passwordConf: req.body.passwordConf,
+        }
+
+        User.create(userData, function (error, user) {
+            if (error) {
+                return next(error);
+            } else {
+                req.session.userId = user._id;
+                return res.redirect('/profile');
+            }
+        });
+
+    } else if (req.body.logemail && req.body.logpassword) {
+        User.authenticate(req.body.logemail, req.body.logpassword, function (error, user) {
+            if (error || !user) {
+                var err = new Error('Wrong email or password.');
+                err.status = 401;
+                return next(err);
+            } else {
+                req.session.userId = user._id;
+                return res.redirect('/profile');
+            }
+        });
+    } else {
+        var err = new Error('All fields required.');
+        err.status = 400;
+        return next(err);
+    }
+
+})
+
+app.get('/profile', (req, res, next) => {
+    User.findById(req.session.userId)
+        .exec(function (error, user) {
+            if (error) {
+                return next(error);
+            } else {
+                if (user === null) {
+                    var err = new Error('Not authorized! Go back!');
+                    err.status = 400;
+                    return next(err);
+                } else {
+                    return res.send('<h1>Name: </h1>' + user.username + '<h2>Mail: </h2>' + user.email + '<br><a type="button" href="/logout">Logout</a>')
+                }
+            }
+        });
+})
+
+app.get('/logout', (req, res, next) => {
+    if (req.session) {
+        // delete session object
+        req.session.destroy(function (err) {
+            if (err) {
+                return next(err);
+            } else {
+                return res.redirect('/');
+            }
+        });
+    }
+})
+//End of the routes for authentication
+
+
 app.post('/medications', (req, res) => {
     db.db('medication-reminder').collection('medications').save(req.body, (err, result) => {
         if (err) return console.log(err)
@@ -40,10 +127,10 @@ app.post('/medications', (req, res) => {
     })
 })
 
-app.delete('/medications', (req, res) => {  
-    db.db('medication-reminder').collection('medications').findOneAndDelete({ medication: req.body.del_medication},
-    (err, result) => {
-      if (err) {return res.status(500).send(err)}
-      res.send({message: 'A medication was deleted'})
-    })
-  })
+app.delete('/medications', (req, res) => {
+    db.db('medication-reminder').collection('medications').findOneAndDelete({ medication: req.body.del_medication },
+        (err, result) => {
+            if (err) { return res.status(500).send(err) }
+            res.send({ message: 'A medication was deleted' })
+        })
+})
